75bf07906a4b549c694d1a27c073f47a19857aa3
David Vilela <dvilelaf@gmail.com>
2024-01-29 19:21:42 +0100

    chore: first backend files

---
+++ backend/controller.py
@@ -0,0 +1,150 @@
+from autonomy.cli.helpers.registry import fetch_service_remote
+from autonomy.cli.helpers.image import build_image
+from aea.configurations.data_types import PublicId
+from aea.cli.generate_key import _generate_multiple_keys
+from autonomy.deploy.build import generate_deployment
+from pathlib import Path
+from autonomy.deploy.generators.docker_compose.base import DockerComposeGenerator
+from autonomy.cli.helpers.deployment import stop_deployment, run_deployment, _build_dirs
+import os
+import yaml
+import petname
+import json
+
+
+# install docker compose, tendermint
+
+class Controller:
+
+    def __init__(self) -> None:
+
+        # Load config
+        with open(Path("operate.yaml"), "r") as config_file:
+            self.config = [doc for doc in yaml.safe_load_all(config_file)][0]
+
+        # Set dirs
+        self.app_dir = Path().absolute()
+        self.data_dir = Path(self.app_dir, "data")
+        self.services_dir = Path(self.data_dir, "services")
+        self.keys_dir = Path(self.data_dir, "keys")
+        self.builds_dir = Path(self.data_dir, "builds")
+
+        # Create dirs
+        for d in [self.data_dir, self.services_dir, self.keys_dir, self.builds_dir]:
+            Path(d).mkdir(parents=True, exist_ok=True)
+
+        # Load keys
+        self.key_names = [k.stem for k in self.keys_dir.iterdir()]
+
+        # Create master key
+        self.create_keys("master_key")
+
+        # Download services
+        self.fetch_services()
+
+
+    def get_services(self):
+        return self.config["services"]
+
+    def get_service_dir(self, service_id):
+        service_id = PublicId.from_str(service_id)
+        return Path(self.services_dir, str(service_id).replace("/", ":"))
+
+    def get_build_dir(self, service_id):
+        service_id = PublicId.from_str(service_id)
+        return Path(self.builds_dir, str(service_id).replace("/", ":"))
+
+    def get_keys(self):
+        return self.key_names
+
+    def create_keys_file(self, key_names, target_dir):
+        keys = []
+        for key_name in key_names:
+            with open(Path(self.keys_dir, f"{key_name}.json"), "r") as key_file:
+                keys.append(json.load(key_file)[0])
+
+        keys_path = Path(target_dir, "keys.json")
+        with open(keys_path, "w") as keys_file:
+            json.dump(keys, keys_file, indent=4)
+
+        return keys_path
+
+    def create_keys(self, name=None, type_="ethereum"):
+        name = name or petname.Generate(2)
+        name += ".json"
+        keys_path = Path(self.keys_dir, name)
+        if not Path(keys_path).is_file():
+            _generate_multiple_keys(n=1, type_=type_, password=None, file=keys_path)
+        return name
+
+    def fetch_services(self):
+        for service_id in self.config["services"].keys():
+            service_dir = self.get_service_dir(service_id)
+            if not os.path.isdir(service_dir):
+                fetch_service_remote(service_id, service_dir)
+
+
+    def get_service_config(self, service_id):
+        service_dir = self.get_service_dir(service_id)
+        with open(Path(service_dir, "service.yaml"), "r") as service_config_file:
+            service_config = [doc for doc in yaml.safe_load_all(service_config_file)]
+            return service_config
+
+    def get_vars(self, service_id):
+        service_config = self.get_service_config(service_id)
+        return service_config[1][0]["models"]["params"]["args"]
+
+
+    def build_deployment(self, service_id, key_names, env_vars=None):
+        service_dir = self.get_service_dir(service_id)
+        build_dir = self.get_build_dir(service_id)
+
+        # Load the service config
+        service_config = self.get_service_config(service_id)
+
+        # Build docker image
+        build_image(
+            agent=PublicId.from_str(service_config[0]["agent"]),
+            service_dir=service_dir
+        )
+
+        # Build deployment. TODO: remove folder if exists
+        Path(build_dir).mkdir(parents=True, exist_ok=True)
+        _build_dirs(build_dir)
+
+        # Create keys file
+        keys_file = self.create_keys_file(key_names, build_dir)
+
+        # Create env file
+        if env_vars:
+            env_file = Path(build_dir, ".env")
+            with open(env_file, "w") as env_file:
+                env_file.writelines([f"{k}={v}" for k, v in env_vars.items()])
+
+        # Generate deployment
+        generate_deployment(
+            type_of_deployment=DockerComposeGenerator.deployment_type,
+            keys_file=keys_file,
+            service_path=service_dir,
+            build_dir=build_dir,
+            number_of_agents=1,
+            use_tm_testnet_setup=True,
+        )
+
+
+    def start_service(self, service_id):
+        build_dir = self.get_build_dir(service_id)
+        run_deployment(
+            build_dir=build_dir,
+            no_recreate=False,
+            remove_orphans=False,
+            detach=True
+        )
+
+    def stop_service(self, service_id):
+        build_dir = self.get_build_dir(service_id)
+        stop_deployment(build_dir)
+
+
+controller = Controller()
+controller.build_deployment("valory/trader:0.1.0:bafybeifhq2udyttnuidkc7nmtjcfzivbbnfcayixzps7fa5x3cg353bvfe", ["divine-bee"])
---
+++ backend/server.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2021-2023 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+"""This module implements the Olas Operate App backend endpoints."""
+
+
+from flask import Flask
+from controller import Controller
+
+
+def create_app():
+
+    controller = Controller()
+    operate = Flask(__name__)
+
+    # Get services
+    @operate.route("/services", methods=["GET"])
+    def get_services():
+        return controller.get_services()
+
+    # Get keys
+    @operate.route("/keys", methods=["GET"])
+    def get_mentions():
+        return controller.get_keys()
+
+    # Create keys
+    @operate.route("/keys", methods=["POST"])
+    def create_keys():
+        return controller.create_keys()
+
+    # Get vars
+    @operate.route("/vars/<service_id>", methods=["GET"])
+    def get_vars(service_id):
+        return controller.get_vars(service_id)
+
+    # Start service
+    @operate.route("/start_service/<service_id>", methods=["POST"])
+    def start_service(service_id):
+        return controller.start_service(service_id)
+
+    # Stop service
+    @operate.route("/stop_service/<service_id>", methods=["POST"])
+    def stop_service(service_id):
+        return controller.stop_service(service_id)
+
+    return operate
+
+
+if __name__ == "__main__":
+    operate = create_app()
+    operate.run(debug=True, host="0.0.0.0")
