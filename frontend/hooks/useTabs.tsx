12329f739ba5be78633c650127bb7c6f225ee355
Josh Miller <joshua.miller@valory.xyz>
2024-02-05 19:07:49 +0000

    chore: use enums for tabs

--- frontend/hooks/useTabs.tsx
+++ frontend/hooks/useTabs.tsx
@@ -1,6 +1,11 @@
 import { TabsContext } from "@/context/TabsProvider";
 import { useContext } from "react";
 
+export enum Tab {
+  YOUR_AGENTS = "1",
+  MARKETPLACE = "2",
+}
+
 export const useTabs = () => {
   return useContext(TabsContext);
 };
