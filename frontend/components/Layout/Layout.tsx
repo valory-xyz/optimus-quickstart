77dc0ffa959362a556b56af9930a69cbd673ef1d
Josh Miller <joshua.miller@valory.xyz>
2024-02-05 19:07:00 +0000

    feature: added layout components i.e navbar

--- frontend/components/Layout/Layout.tsx
+++ frontend/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import { PropsWithChildren } from "react";
+import { Navbar } from "./Navbar";
+import { Flex } from "antd";
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <>
-      <nav></nav>
+    <Flex vertical>
+      <Navbar />
       {children}
-    </>
+    </Flex>
   );
 };
