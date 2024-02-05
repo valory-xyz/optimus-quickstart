966214852d276406fa567e09211067f1889736c1
Josh Miller <joshua.miller@valory.xyz>
2024-02-05 17:35:18 +0000

    feat: provider boilerplate

---
+++ frontend/context/AgentsProvider.tsx
@@ -0,0 +1,13 @@
+import { Dispatch, PropsWithChildren, SetStateAction, createContext, useState } from "react";
+
+type AgentContextType = {
+    agents: any[];
+    setAgents: Dispatch<SetStateAction<any[]>>;
+};
+
+export const AgentsContext = createContext<AgentContextType>({agents: [], setAgents: ()=>{}});
+
+export const AgentsProvider = ({ children }: PropsWithChildren) => {
+    const [agents, setAgents] = useState<any[]>([]);
+    return <AgentsContext.Provider value={{agents, setAgents}}>{children}</AgentsContext.Provider>;
+};
