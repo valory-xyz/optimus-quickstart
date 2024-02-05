e0cdd0840e10489702ad4656214b407a737d8d28
Josh Miller <mlxndrj@gmail.com>
2024-02-05 22:50:12 +0000

    Add Docker commands to run and stop agents

--- frontend/pages/api/agents/run.tsx
+++ frontend/pages/api/agents/run.tsx
@@ -1,7 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import {exec} from "child_process";
+
+
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: "Success" });
+  try {
+     exec('docker run -p 1337 hello-world',(error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        res.status(500).json({ message: error });
+      }
+      res.status(200).json({ message: stdout });
+    });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+  res.status(500).json({ message: "Failed" });
+
 };
 
 export default handler;
