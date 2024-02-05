a32983b128dff2f19ea6cfbb9b0c272910059750
Josh Miller <joshua.miller@valory.xyz>
2024-02-05 17:55:56 +0000

    fix: lint your agents

--- frontend/components/YourAgents/YourAgents.tsx
+++ frontend/components/YourAgents/YourAgents.tsx
@@ -2,9 +2,13 @@ import { useTabs } from "@/hooks/useTabs";
 import { Button } from "antd";
 
 export const YourAgents = () => {
-    const {setActiveTab} = useTabs();
-    return <>
-    <p>No agents running.</p> 
-    <Button type="primary" onClick={()=>setActiveTab("2")}>Browse Agents</Button>
-    </>;
+  const { setActiveTab } = useTabs();
+  return (
+    <>
+      <p>No agents running.</p>
+      <Button type="primary" onClick={() => setActiveTab("2")}>
+        Browse Agents
+      </Button>
+    </>
+  );
 };
