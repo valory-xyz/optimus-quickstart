98c278d0abb01446d0456b457e762b7d23d1086d
Josh Miller <mlxndrj@gmail.com>
2024-02-07 13:43:22 +0000

    Add NotificationButton component

---
+++ components/Layout/Navbar/NotificationButton/NotificationButton.tsx
@@ -0,0 +1,16 @@
+import { BellOutlined } from "@ant-design/icons";
+import { MenuProps, Dropdown, Button } from "antd";
+
+const items: MenuProps["items"] = [];
+
+type NotificationButtonProps = {
+  disabled?: boolean;
+};
+
+export const NotificationButton = ({ disabled }: NotificationButtonProps) => {
+  return (
+    <Dropdown menu={{ items }} placement="bottomLeft" disabled={disabled}>
+      <Button type="text" icon={<BellOutlined />} />
+    </Dropdown>
+  );
+};
