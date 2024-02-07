16c916fcb13f0f923d0063d5ac773cc8a278d1e6
Josh Miller <mlxndrj@gmail.com>
2024-02-07 14:32:15 +0000

    Add SpawnLoading component

--- components/Spawn/index.tsx
+++ components/Spawn/index.tsx
@@ -2,3 +2,4 @@ export * from "./SpawnDone/SpawnDone";
 export * from "./SpawnFunds/SpawnFunds";
 export * from "./SpawnHeader/SpawnHeader";
 export * from "./SpawnRPC/SpawnRPC";
+export * from "./SpawnLoading/SpawnLoading";
