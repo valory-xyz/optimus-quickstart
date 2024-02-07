f183b53ab691ef7dd83c8e517616bc6b0d2731f7
Josh Miller <mlxndrj@gmail.com>
2024-02-07 22:28:39 +0000

    Remove unused API handlers

--- pages/api/agents/index.tsx
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  const agents: any[] = [];
-  res.status(200).json({ agents });
-};
-
-export default handler;
