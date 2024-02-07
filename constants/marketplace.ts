9264229af4a53b223288cfa517ab1ed8744c3461
Josh Miller <mlxndrj@gmail.com>
2024-02-07 13:45:30 +0000

    Add marketplace constants

---
+++ constants/marketplace.ts
@@ -0,0 +1,11 @@
+import { MarketplaceItem } from "@/types/MarketplaceItem";
+
+export const MARKETPLACE_ITEMS: MarketplaceItem[] = [
+  {
+    id: "1",
+    name: "Prediction Agent",
+    description:
+      "Participates in prediction markets according to your strategy.",
+    image_src: "/marketplace/prediction-agent.png",
+  },
+];
