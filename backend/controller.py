b686a231dd5416eeaf417017e4842db18e044824
David Vilela <dvilelaf@gmail.com>
2024-02-07 18:14:08 +0100

    fix: build try except

--- backend/controller.py
+++ backend/controller.py
@@ -49,54 +49,56 @@ class Controller:
         agent_addresses = [manager.keys.create() for _ in range(service_config["number_of_keys"])]
 
         code = HTTP_OK
-        response_json = {"addresses": agent_addresses}
+        response_json = {"fund_requirements": {
+            address: service_config["required_funds"] for address in agent_addresses}
+        }
 
-        # try:
+        try:
 
-        published = manager.mint(
-            phash=service_hash,
-            rpc=rpc,
-            agent_id=service_config["agent_id"],
-            number_of_slots=service_config["number_of_slots"],
-            cost_of_bond=service_config["cost_of_bond"],
-            threshold=service_config["threshold"],
-            nft=IPFSHash(
-                self.config["services"][service_hash]["nft"]
-            ),
-            custom_addresses=custom_addresses,
-        )
+            published = manager.mint(
+                phash=service_hash,
+                rpc=rpc,
+                agent_id=service_config["agent_id"],
+                number_of_slots=service_config["number_of_slots"],
+                cost_of_bond=service_config["cost_of_bond"],
+                threshold=service_config["threshold"],
+                nft=IPFSHash(
+                    self.config["services"][service_hash]["nft"]
+                ),
+                custom_addresses=custom_addresses,
+            )
 
-        manager.activate(
-            phash=service_hash,
-            rpc=rpc,
-            custom_addresses=custom_addresses,
-        )
+            manager.activate(
+                phash=service_hash,
+                rpc=rpc,
+                custom_addresses=custom_addresses,
+            )
 
-        manager.register(
-            phash=service_hash,
-            instances=agent_addresses,
-            agents=[14],
-            rpc=rpc,
-            custom_addresses=custom_addresses,
-        )
+            manager.register(
+                phash=service_hash,
+                instances=agent_addresses,
+                agents=[14],
+                rpc=rpc,
+                custom_addresses=custom_addresses,
+            )
 
-        manager.deploy(
-            phash=service_hash,
-            reuse_multisig=False,
-            rpc=rpc,
-            custom_addresses=custom_addresses,
-        )
+            manager.deploy(
+                phash=service_hash,
+                reuse_multisig=False,
+                rpc=rpc,
+                custom_addresses=custom_addresses,
+            )
 
-        manager.build(
-            phash=service_hash,
-            environment={},
-            volumes={"data": "/data"},
-        )
+            manager.build(
+                phash=service_hash,
+                environment={},
+                volumes={"data": "/data"},
+            )
 
-        # except Exception as e:
-        #     logging.error(str(e))
-        #     code = HTTP_SERVER_ERROR
-        #     response_json = {"error": str(e)}
+        except Exception as e:
+            logging.error(str(e))
+            code = HTTP_SERVER_ERROR
+            response_json = {"error": str(e)}
 
         return response_json, code
 
@@ -115,12 +117,3 @@ class Controller:
         manager = ServiceManager()
         manager.stop(phash=service_hash)
         return {}, HTTP_OK
-
-
-# controller = Controller()
-# controller.build_deployment(
-#     "bafybeifhq2udyttnuidkc7nmtjcfzivbbnfcayixzps7fa5x3cg353bvfe",
-#     {"rpc": "http://localhost:8545"}
-# )
-# controller.start_service("bafybeifhq2udyttnuidkc7nmtjcfzivbbnfcayixzps7fa5x3cg353bvfe")
-# controller.stop_service("bafybeifhq2udyttnuidkc7nmtjcfzivbbnfcayixzps7fa5x3cg353bvfe")
