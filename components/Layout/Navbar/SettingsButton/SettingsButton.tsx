bbbe12ee90100eb11af404bb1be9316e7886f1b7
Josh Miller <mlxndrj@gmail.com>
2024-02-07 13:43:53 +0000

    Add SettingsButton component to Navbar

---
+++ components/Layout/Navbar/SettingsButton/SettingsButton.tsx
@@ -0,0 +1,30 @@
+import { useModals } from "@/hooks/useModals";
+import { SettingOutlined } from "@ant-design/icons";
+import { Button, Dropdown, MenuProps } from "antd";
+import { useMemo } from "react";
+
+export const SettingsButton = () => {
+  const { rpcModalOpen, setRpcModalOpen } = useModals();
+
+  const items: MenuProps["items"] = useMemo(
+    () => [
+      {
+        key: "1",
+        label: "RPCs",
+        onClick: () => setRpcModalOpen(true),
+      },
+      {
+        key: "2",
+        label: "Agent Keys",
+        disabled: true,
+      },
+    ],
+    [setRpcModalOpen],
+  );
+
+  return (
+    <Dropdown menu={{ items }} placement="bottomLeft">
+      <Button type="text" icon={<SettingOutlined />} />
+    </Dropdown>
+  );
+};
