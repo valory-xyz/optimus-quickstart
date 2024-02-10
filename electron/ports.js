24616fb3be5fa8d7d8a37d7dedbb8e56b8269bdd
Josh Miller <mlxndrj@gmail.com>
2024-02-10 20:08:26 +0000

    Add ports.js file for managing port availability and finding available ports

---
+++ electron/ports.js
@@ -0,0 +1,55 @@
+const net = require("net");
+
+const portRange = { startPort: 39152, endPort: 65535 };
+
+const isPortAvailable = async (port) => {
+  return new global.Promise((resolve, reject) => {
+    const server = net.createServer();
+
+    server.once("error", (err) => {
+      if (err.code === "EADDRINUSE") {
+        resolve(false);
+      } else {
+        reject(err);
+      }
+    });
+
+    server.once("listening", () => {
+      server.close();
+      resolve(true);
+    });
+
+    server.listen(port, "localhost");
+  });
+};
+
+const findAvailablePort = async (startPort, endPort) => {
+  return new global.Promise((resolve, reject) => {
+    const server = net.createServer();
+
+    server.on("error", (err) => {
+      if (err.code === "EADDRINUSE") {
+        if (startPort < endPort) {
+          findAvailablePort(startPort + 1, endPort)
+            .then(resolve)
+            .catch(reject);
+        } else {
+          reject(new Error("No available ports in the specified range"));
+        }
+      } else {
+        reject(err);
+      }
+    });
+
+    server.on("listening", () => {
+      const port = server.address().port;
+      server.close(() => {
+        resolve(port);
+      });
+    });
+
+    server.listen(startPort, "localhost");
+  });
+};
+
+module.exports = { isPortAvailable, portRange, findAvailablePort };
