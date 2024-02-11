86d63418ba2880f723baf68eb724de6617ee9010
Josh Miller <mlxndrj@gmail.com>
2024-02-11 22:59:10 +0000

    Remove unused code in SettingsButton component

--- components/Layout/Navbar/SettingsButton/SettingsButton.tsx
+++ components/Layout/Navbar/SettingsButton/SettingsButton.tsx
@@ -1,26 +1,9 @@
-import { useModals } from "@/hooks/useModals";
 import { SettingOutlined } from "@ant-design/icons";
 import { Button, Dropdown, MenuProps } from "antd";
 import { useMemo } from "react";
 
 export const SettingsButton = ({ disabled }: { disabled: boolean }) => {
-  const { setRpcModalOpen } = useModals();
-
-  const items: MenuProps["items"] = useMemo(
-    () => [
-      {
-        key: "rpcs",
-        label: "RPCs",
-        onClick: () => setRpcModalOpen(true),
-      },
-      {
-        key: "agent-keys",
-        label: "Agent Keys",
-        disabled: true,
-      },
-    ],
-    [setRpcModalOpen],
-  );
+  const items: MenuProps["items"] = useMemo(() => [], []);
 
   return (
     <Dropdown menu={{ items }} placement="bottomLeft" disabled={disabled}>
