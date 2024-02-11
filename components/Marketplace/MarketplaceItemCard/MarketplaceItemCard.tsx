a2ea41af5eedbe4ba4a26561341fdc903b19cfb5
Josh Miller <mlxndrj@gmail.com>
2024-02-11 22:59:20 +0000

    Refactor MarketplaceItemCard component to use new Service and ServiceMeta types

--- components/Marketplace/MarketplaceItemCard/MarketplaceItemCard.tsx
+++ components/Marketplace/MarketplaceItemCard/MarketplaceItemCard.tsx
@@ -1,16 +1,37 @@
-import { MarketplaceItem } from "@/types/MarketplaceItem";
+import { Tab } from "@/enums";
+import { useTabs } from "@/hooks/useTabs";
+import { Service } from "@/types/Service";
+import { ServiceMeta } from "@/types/ServiceMeta";
 import { Button, Typography, Flex, Card } from "antd";
 import Image from "next/image";
+import { useMemo } from "react";
 export const MarketplaceItemCard = ({
-  marketplaceItem,
-  serviceHash,
+  serviceMeta,
+  service,
   marginBottom,
 }: {
-  marketplaceItem: MarketplaceItem;
-  serviceHash: string;
+  serviceMeta: ServiceMeta;
+  service: Service;
   marginBottom?: number;
 }) => {
-  const { name, description, image_src } = marketplaceItem;
+  const { name, description, image_src } = serviceMeta;
+  const { setActiveTab } = useTabs();
+
+  const button = useMemo(() => {
+    if (service.running) {
+      return (
+        <Button type="primary" onClick={() => setActiveTab(Tab.YOUR_AGENTS)}>
+          View Agent
+        </Button>
+      );
+    }
+    return (
+      <Button type="primary" href={`/spawn/${service.hash}`}>
+        Run Agent
+      </Button>
+    );
+  }, [service.hash, service.running, setActiveTab]);
+
   return (
     <Card style={{ marginBottom }}>
       <Flex gap={16}>
@@ -19,11 +40,7 @@ export const MarketplaceItemCard = ({
           <Flex vertical style={{ height: "100%" }}>
             <Typography.Title level={3}>{name}</Typography.Title>
             <Typography.Text>{description}</Typography.Text>
-            <Flex style={{ marginTop: "auto" }}>
-              <Button href={`/spawn/${serviceHash}`} type="primary">
-                Run this agent
-              </Button>
-            </Flex>
+            <Flex style={{ marginTop: "auto" }}>{button}</Flex>
           </Flex>
         </Flex>
       </Flex>
