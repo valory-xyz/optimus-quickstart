68dcf54b70ff739403838fde230162c94cd80a94
Josh Miller <mlxndrj@gmail.com>
2024-02-15 15:30:53 +0000

    Update backend URL in urls.ts

--- frontend/constants/urls.ts
+++ frontend/constants/urls.ts
@@ -1,2 +1,2 @@
 export const NODIES_URL = "https://nodies.app";
-export const BACKEND_URL = `http://localhost:${process.env.NEXT_PUBLIC_BACKEND_PORT}`;
+export const BACKEND_URL = `http://localhost:${process.env.NEXT_PUBLIC_BACKEND_PORT}/api`;
