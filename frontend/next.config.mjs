5d4ea64f9bce6e8068c29101d48ea7e2e559afed
Josh Miller <mlxndrj@gmail.com>
2024-02-15 15:30:25 +0000

    Add next/babel to .eslintrc.json and update images configuration in next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-     domains: ['*'],
+     remotePatterns: [{
+      protocol: "https",
+      hostname: "**"
+     }],
   },
   reactStrictMode: true,
   transpilePackages: [
