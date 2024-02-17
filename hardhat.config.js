344d7eb82631056c32af4b1cc66ef24758044785
Josh Miller <mlxndrj@gmail.com>
2024-02-17 22:40:44 +0000

    Add port configuration to hardhat network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,13 @@
 // temporary during development
+require("dotenv").config();
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       forking: {url: "https://lb.nodies.app/v1/406d8dcc043f4cb3959ed7d6673d311a"},
       chainId: 100,
+      port: process.env.PORT || 8545,      
     },
   },
+  
 };
