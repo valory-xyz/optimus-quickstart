9794eacaafc39903f73ba8cfeb6bef810749f10a
Josh Miller <mlxndrj@gmail.com>
2024-02-18 13:15:04 +0000

    Add QR modal functionality to useModals hook

--- frontend/hooks/useModals.tsx
+++ frontend/hooks/useModals.tsx
@@ -1,6 +1,22 @@
 import { ModalsContext } from "@/context";
+import { QRModalData } from "@/types/QRModalData";
 import { useContext } from "react";
 
 export const useModals = () => {
-  return useContext(ModalsContext);
+  const { qrModalData, setQrModalData } = useContext(ModalsContext);
+
+  //  QR MODALS
+
+  const qrModalReset = () =>
+    setQrModalData({
+      open: false,
+      address: undefined,
+      amount: undefined,
+      chainId: undefined,
+    });
+
+  const qrModalOpen = (data: Omit<Required<QRModalData>, "open">) =>
+    setQrModalData({ open: true, ...data });
+
+  return { qrModalData, setQrModalData, qrModalReset, qrModalOpen };
 };
