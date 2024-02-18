170bf4afb56ea6edd420574bed7163f4f440dc02
Josh Miller <mlxndrj@gmail.com>
2024-02-18 11:39:44 +0000

    Add useMulticall hook for fetching ETH balances

---
+++ frontend/hooks/useMulticall.tsx
@@ -0,0 +1,38 @@
+import { MULTICALL } from "@/constants/contracts";
+import { BigNumber, ethers } from "ethers";
+import { Contract, Provider } from "ethers-multicall";
+import { multicall3Abi } from "@/abi/multicall3Abi";
+
+export const useMulticall = () => {
+  const getETHBalances = async (
+    addresses: string[],
+    rpc: string,
+  ): Promise<{ [address: string]: number }> => {
+    const provider = new ethers.providers.JsonRpcProvider(rpc);
+    const multicallProvider = new Provider(provider, 100);
+
+    const multicallContract = new Contract(MULTICALL, multicall3Abi);
+
+    const callData = addresses.map((address) => {
+      console.log(address);
+      console.log(multicallContract.getEthBalance(address));
+      return multicallContract.getEthBalance(address);
+    });
+
+    return multicallProvider.all(callData).then((r: BigNumber[]) =>
+      r.reduce(
+        (
+          acc: { [address: string]: number },
+          balance: BigNumber,
+          index: number,
+        ) => ({
+          ...acc,
+          [addresses[index]]: parseFloat(ethers.utils.formatEther(balance)),
+        }),
+        {},
+      ),
+    );
+  };
+
+  return { getETHBalances };
+};
