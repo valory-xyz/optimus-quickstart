04867bc96ecc8344dd0456cd4c72c258113f542e
Josh Miller <mlxndrj@gmail.com>
2024-02-19 13:34:31 +0000

    Refactor useServices hook to handle error messages

--- frontend/hooks/useServices.tsx
+++ frontend/hooks/useServices.tsx
@@ -1,10 +1,4 @@
-import {
-  DeleteServicesResponse,
-  Deployment,
-  Service,
-  ServiceHash,
-  ServiceTemplate,
-} from "@/client";
+import { Service, ServiceHash, ServiceTemplate } from "@/client";
 import { ServicesContext } from "@/context";
 import ServicesService from "@/service/Services";
 import { message } from "antd";
@@ -15,26 +9,35 @@ export const useServices = () => {
     useContext(ServicesContext);
 
   // SERVICES SERVICE METHODS
-  const createService = async (
-    serviceTemplate: Required<ServiceTemplate>,
-  ): Promise<Service> => ServicesService.createService(serviceTemplate);
+  const createService = async (serviceTemplate: Required<ServiceTemplate>) =>
+    ServicesService.createService(serviceTemplate).catch(() => {
+      message.error("Failed to create service");
+    });
 
   const deployService = async (serviceHash: ServiceHash) =>
-    ServicesService.deployService(serviceHash);
+    ServicesService.deployService(serviceHash).catch(() => {
+      message.error("Failed to deploy service");
+    });
 
   const stopService = async (serviceHash: string) =>
-    ServicesService.stopService(serviceHash);
+    ServicesService.stopService(serviceHash).catch(() => {
+      message.error("Failed to stop service");
+    });
 
-  const deleteServices = async (
-    hashes: ServiceHash[],
-  ): Promise<DeleteServicesResponse> =>
-    ServicesService.deleteServices({ hashes });
+  const deleteServices = async (hashes: ServiceHash[]) =>
+    ServicesService.deleteServices({ hashes }).catch(() => {
+      message.error("Failed to delete services");
+    });
 
-  const getService = (serviceHash: ServiceHash): Promise<Service> =>
-    ServicesService.getService(serviceHash);
+  const getService = (serviceHash: ServiceHash) =>
+    ServicesService.getService(serviceHash).catch(() =>
+      message.error("Failed to get service"),
+    );
 
-  const getServiceStatus = (serviceHash: ServiceHash): Promise<Deployment> =>
-    ServicesService.getServiceStatus(serviceHash);
+  const getServiceStatus = (serviceHash: ServiceHash) =>
+    ServicesService.getServiceStatus(serviceHash).catch(() =>
+      message.error("Failed to get service status"),
+    );
 
   // STATE METHODS
   const getServiceFromState = (
