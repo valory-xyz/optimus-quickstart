eafbc9309b51f3cf874782eedfb0af04bab31ecf
Josh Miller <mlxndrj@gmail.com>
2024-02-20 16:17:45 +0000

    Refactor useSpawn hook to remove isStaking parameter

--- frontend/hooks/useSpawn.tsx
+++ frontend/hooks/useSpawn.tsx
@@ -2,30 +2,16 @@ import { SpawnContext } from "@/context";
 import { SpawnScreenState } from "@/enums";
 import { useCallback, useContext, useMemo } from "react";
 
-export const useSpawn = (isStaking?: boolean) => {
+export const useSpawn = () => {
   const { spawnScreenState, setSpawnScreenState, firstSpawnScreenState } =
     useContext(SpawnContext);
 
   const spawnPercentage: number = useMemo(() => {
-    if (spawnScreenState === SpawnScreenState.STAKING_CHECK) return 0;
     // Staking path
-    if (isStaking) {
-      switch (spawnScreenState) {
-        case SpawnScreenState.RPC:
-          return 25;
-        case SpawnScreenState.STAKING_FUNDING:
-          return 50;
-        case SpawnScreenState.AGENT_FUNDING:
-          return 75;
-        case SpawnScreenState.DONE:
-          return 100;
-        default:
-          break;
-      }
-    }
-    // Non-staking path
     switch (spawnScreenState) {
       case SpawnScreenState.RPC:
+        return 0;
+      case SpawnScreenState.STAKING_CHECK:
         return 33;
       case SpawnScreenState.AGENT_FUNDING:
         return 66;
@@ -35,7 +21,7 @@ export const useSpawn = (isStaking?: boolean) => {
         break;
     }
     return 0;
-  }, [isStaking, spawnScreenState]);
+  }, [spawnScreenState]);
 
   const resetSpawnScreenState = useCallback(
     (): void => setSpawnScreenState(firstSpawnScreenState),
