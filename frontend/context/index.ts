fa19172ace565b246234b00f1270fbc223dd9053
Josh Miller <mlxndrj@gmail.com>
2024-02-20 22:56:08 +0000

    Add AppInfoProvider to context

--- frontend/context/index.ts
+++ frontend/context/index.ts
@@ -2,3 +2,4 @@ export * from "./ModalsProvider/ModalsProvider";
 export * from "./ServicesProvider/ServicesProvider";
 export * from "./SpawnProvider/SpawnProvider";
 export * from "./TabsProvider/TabsProvider";
+export * from "./AppInfoProvider/AppInfoProvider";
