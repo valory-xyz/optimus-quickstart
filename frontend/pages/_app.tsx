e5cf24cb2c6d20fb5c78dd4447acbb5334a84056
Josh Miller <mlxndrj@gmail.com>
2024-02-20 16:18:55 +0000

    Add AppInfoProvider to _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,22 +4,25 @@ import {
   SpawnProvider,
   TabsProvider,
 } from "@/context";
+import { AppInfoProvider } from "@/context/AppInfoProvider/AppInfoProvider";
 import { mainTheme } from "@/theme/mainTheme";
 import { ConfigProvider } from "antd";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ServicesProvider>
-      <SpawnProvider>
-        <ModalsProvider>
-          <TabsProvider>
-            <ConfigProvider theme={mainTheme}>
-              <Component {...pageProps} />
-            </ConfigProvider>
-          </TabsProvider>
-        </ModalsProvider>
-      </SpawnProvider>
-    </ServicesProvider>
+    <AppInfoProvider>
+      <ServicesProvider>
+        <SpawnProvider>
+          <ModalsProvider>
+            <TabsProvider>
+              <ConfigProvider theme={mainTheme}>
+                <Component {...pageProps} />
+              </ConfigProvider>
+            </TabsProvider>
+          </ModalsProvider>
+        </SpawnProvider>
+      </ServicesProvider>
+    </AppInfoProvider>
   );
 }
