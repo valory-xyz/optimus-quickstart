946cebc32265d9e89ccef242e7c23f1f60ec1ff3
Josh Miller <mlxndrj@gmail.com>
2024-03-01 19:10:54 +0000

    Add SpawnScreen enum

---
+++ frontend/enums/SpawnScreen.ts
@@ -0,0 +1,8 @@
+export enum SpawnScreen {
+  STAKING_CHECK = 'staking-check',
+  RPC = 'rpc',
+  AGENT_FUNDING = 'agent-funding',
+  DONE = 'done',
+  MASTER_WALLET_FUNDING = 'master-wallet-funding',
+  ERROR = 'error',
+}
