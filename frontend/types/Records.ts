7a50a81c129381549726baab9a3a6a5c71c252f3
Josh Miller <mlxndrj@gmail.com>
2024-03-06 10:48:22 +0000

    Add FundingRecord type to Records.ts

--- frontend/types/Records.ts
+++ frontend/types/Records.ts
@@ -2,3 +2,7 @@ import { Address } from '.';
 
 export type AddressNumberRecord = Record<Address, number>;
 export type AddressBooleanRecord = Record<Address, boolean>;
+export type FundingRecord = Record<
+  Address,
+  { required: number; received: boolean }
+>;
