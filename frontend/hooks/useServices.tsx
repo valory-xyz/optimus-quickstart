cb8224ebae0596b1799beca10017b1765bd14e8a
Josh Miller <mlxndrj@gmail.com>
2024-03-06 10:47:59 +0000

    Refactor useServices hook to remove unused methods

--- frontend/hooks/useServices.tsx
+++ frontend/hooks/useServices.tsx
@@ -1,4 +1,4 @@
-import { Service, ServiceHash, ServiceTemplate } from '@/client';
+import { Service, ServiceHash } from '@/client';
 import { ServicesContext } from '@/context';
 import { ServicesService } from '@/service';
 import { useContext } from 'react';
@@ -6,25 +6,6 @@ import { useContext } from 'react';
 export const useServices = () => {
   const { services, updateServicesState, hasInitialLoaded, setServices } =
     useContext(ServicesContext);
-
-  // SERVICES SERVICE METHODS
-  const createService = async (serviceTemplate: Required<ServiceTemplate>) =>
-    ServicesService.createService(serviceTemplate);
-
-  const deployService = async (serviceHash: ServiceHash) =>
-    ServicesService.deployService(serviceHash);
-
-  const stopService = async (serviceHash: string) =>
-    ServicesService.stopService(serviceHash);
-
-  const deleteServices = async (hashes: ServiceHash[]) =>
-    ServicesService.deleteServices({ hashes });
-
-  const getService = async (serviceHash: ServiceHash) =>
-    ServicesService.getService(serviceHash);
-
-  const getServiceStatus = async (serviceHash: ServiceHash) =>
-    ServicesService.getServiceStatus(serviceHash);
 
   // STATE METHODS
   const getServiceFromState = (
@@ -40,7 +21,7 @@ export const useServices = () => {
     hasInitialLoaded ? services : [];
 
   const updateServiceState = (serviceHash: ServiceHash) =>
-    getService(serviceHash).then((service: Service) =>
+    ServicesService.getService(serviceHash).then((service: Service) =>
       setServices((prev) => {
         const index = prev.findIndex((s) => s.hash === serviceHash); // findIndex returns -1 if not found
         if (index === -1) return [...prev, service];
@@ -54,17 +35,11 @@ export const useServices = () => {
     setServices((prev) => prev.filter((s) => s.hash !== serviceHash));
 
   return {
-    getService,
     getServiceFromState,
     getServicesFromState,
-    getServiceStatus,
     updateServicesState,
     updateServiceState,
     deleteServiceState,
-    createService,
-    deployService,
-    stopService,
-    deleteServices,
     hasInitialLoaded,
   };
 };
