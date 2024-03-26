669fdc2648a43c1b53f243a178bc30b8299c698e
Josh Miller <mlxndrj@gmail.com>
2024-03-26 15:36:53 +0000

    Refactor imports in _app.tsx

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,9 +3,12 @@ import '../styles/globals.scss';
 import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
 
-import { AppInfoProvider, ServicesProvider } from '@/context';
-import { PageStateProvider } from '@/context/PageStateProvider';
-import { SetupProvider } from '@/context/SetupProvider';
+import {
+  AppInfoProvider,
+  PageStateProvider,
+  ServicesProvider,
+  SetupProvider,
+} from '@/context';
 import { mainTheme } from '@/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
