f9c555ac572ccb05e2769bcc4b345b968a6399ae
angrybayblade <vptl185@gmail.com>
2024-03-27 10:29:33 +0530

    feat: script to setup master wallet

--- scripts/fund.py
+++ scripts/fund.py
@@ -9,62 +9,68 @@ from aea_ledger_ethereum.ethereum import EthereumApi, EthereumCrypto
 from dotenv import load_dotenv
 
 
+load_dotenv()
+
 OLAS_CONTRACT_ADDRESS_GNOSIS = "0xcE11e14225575945b8E6Dc0D4F2dD4C570f79d9f"
 
-load_dotenv()
-address = sys.argv[1]
-staking_keys_path = os.environ.get("STAKING_TEST_KEYS_PATH", None)
-ledger_api = EthereumApi(address="http://localhost:8545")
-crypto = EthereumCrypto("scripts/keys/gnosis.txt")
-tx = ledger_api.get_transfer_transaction(
-    sender_address=crypto.address,
-    destination_address=address,
-    amount=10000000000000000000,
-    tx_fee=50000,
-    tx_nonce="0x",
-    chain_id=100,
-)
-stx = crypto.sign_transaction(transaction=tx)
-digest = ledger_api.send_signed_transaction(stx)
-ledger_api.get_transaction_receipt(tx_digest=digest, raise_on_try=True)
 
-print(ledger_api.get_balance(address=address))
-
-if staking_keys_path:
-    staking_crypto = EthereumCrypto(staking_keys_path)
-
-    with open(
-        Path(
-            "operate",
-            "data",
-            "contracts",
-            "uniswap_v2_erc20",
-            "build",
-            "IUniswapV2ERC20.json",
-        ),
-        "r",
-        encoding="utf-8",
-    ) as abi_file:
-        abi = json.load(abi_file)["abi"]
-
-    olas_contract = ledger_api.api.eth.contract(
-        address=ledger_api.api.to_checksum_address(OLAS_CONTRACT_ADDRESS_GNOSIS),
-        abi=abi,
+def fund(address: str) -> None:
+    """Fund an address."""
+    staking_keys_path = os.environ.get("STAKING_TEST_KEYS_PATH", None)
+    ledger_api = EthereumApi(address="http://localhost:8545")
+    crypto = EthereumCrypto("scripts/keys/gnosis.txt")
+    tx = ledger_api.get_transfer_transaction(
+        sender_address=crypto.address,
+        destination_address=address,
+        amount=10000000000000000000,
+        tx_fee=50000,
+        tx_nonce="0x",
+        chain_id=100,
     )
+    stx = crypto.sign_transaction(transaction=tx)
+    digest = ledger_api.send_signed_transaction(stx)
+    ledger_api.get_transaction_receipt(tx_digest=digest, raise_on_try=True)
 
-    tx = olas_contract.functions.transfer(address, int(2e18)).build_transaction(
-        {
-            "chainId": 100,
-            "gas": 100000,
-            "gasPrice": ledger_api.api.to_wei("50", "gwei"),
-            "nonce": ledger_api.api.eth.get_transaction_count(staking_crypto.address),
-        }
-    )
+    print(ledger_api.get_balance(address=address))
+    if staking_keys_path:
+        staking_crypto = EthereumCrypto(staking_keys_path)
+        with open(
+            Path(
+                "operate",
+                "data",
+                "contracts",
+                "uniswap_v2_erc20",
+                "build",
+                "IUniswapV2ERC20.json",
+            ),
+            "r",
+            encoding="utf-8",
+        ) as abi_file:
+            abi = json.load(abi_file)["abi"]
 
-    signed_txn = ledger_api.api.eth.account.sign_transaction(
-        tx, staking_crypto.private_key
-    )
-    tx_hash = ledger_api.api.eth.send_raw_transaction(signed_txn.rawTransaction)
+        olas_contract = ledger_api.api.eth.contract(
+            address=ledger_api.api.to_checksum_address(OLAS_CONTRACT_ADDRESS_GNOSIS),
+            abi=abi,
+        )
 
-    balance = olas_contract.functions.balanceOf(address).call()
-    print(f"Balance of {address} = {balance/1e18} OLAS")
+        tx = olas_contract.functions.transfer(address, int(2e18)).build_transaction(
+            {
+                "chainId": 100,
+                "gas": 100000,
+                "gasPrice": ledger_api.api.to_wei("50", "gwei"),
+                "nonce": ledger_api.api.eth.get_transaction_count(
+                    staking_crypto.address
+                ),
+            }
+        )
+
+        signed_txn = ledger_api.api.eth.account.sign_transaction(
+            tx, staking_crypto.private_key
+        )
+        ledger_api.api.eth.send_raw_transaction(signed_txn.rawTransaction)
+        balance = olas_contract.functions.balanceOf(address).call()
+        print(f"Balance of {address} = {balance/1e18} OLAS")
+
+
+if __name__ == "__main__":
+    fund(sys.argv[1])
---
+++ scripts/setup_wallet.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2024 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Setup wallet."""
+
+import requests
+
+from operate.types import ChainType
+from scripts.fund import fund
+
+
+print("Setting up user account")
+print(
+    requests.post(
+        "http://localhost:8000/api/account",
+        json={
+            "password": "password",
+        },
+    ).json()
+)
+
+print("Logging in")
+print(
+    requests.post(
+        "http://localhost:8000/api/account/login",
+        json={
+            "password": "password",
+        },
+    ).json()
+)
+
+wallet = requests.post(
+    "http://localhost:8000/api/wallet",
+    json={
+        "chain_type": ChainType.GNOSIS,
+    },
+).json()
+print("Setting up wallet")
+print(wallet)
+
+print("Funding wallet: ", end="")
+fund(wallet["wallet"]["address"])
+
+print(
+    requests.put(
+        "http://localhost:8000/api/wallet",
+        json={
+            "chain_type": ChainType.GNOSIS,
+        },
+    ).json()
+)
+
+print(
+    requests.get(
+        "http://localhost:8000/api/wallet",
+    ).json()
+)
