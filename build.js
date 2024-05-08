de50279035e2dad111ac8fc88189a04b28948b5f
Josh Miller <mlxndrj@gmail.com>
2024-05-09 00:05:26 +0100

    chore: Update build workflow to include notarization step for macOS

--- build.js
+++ build.js
@@ -11,32 +11,31 @@ const main = async () => {
     config: {
       productName: 'Olas Operate',
       artifactName: '${productName}-${version}-${platform}-${arch}.${ext}',
+
+      appId: 'xyz.valory.olas-operate-app',
+      category: 'public.app-category.utilities',
+      directories: {
+        output: 'dist',
+      },
       mac: {
-        appId: 'xyz.valory.olas-operate-app',
+        target: [
+          {
+            target: 'dmg',
+            arch: ['arm64'],
+          },
+        ],
+        icon: 'electron/assets/icons/splash-robot-head.png',
+        hardenedRuntime: true,
+        gatekeeperAssess: false,
+        entitlements: 'electron/entitlements.mac.plist',
+        entitlementsInherit: 'electron/entitlements.mac.plist',
         category: 'public.app-category.utilities',
-        directories: {
-          output: 'dist',
-        },
-        mac: {
-          target: [
-            {
-              target: 'dmg',
-              arch: ['arm64'],
-            },
-          ],
-          icon: 'electron/assets/icons/splash-robot-head.png',
-          hardenedRuntime: true,
-          gatekeeperAssess: false,
-          entitlements: 'electron/entitlements.mac.plist',
-          entitlementsInherit: 'electron/entitlements.mac.plist',
-          category: 'public.app-category.utilities',
-        },
-        notarize: {
-          teamId: process.env.APPLETEAMID,
-        },
+      },
+      notarize: {
+        teamId: process.env.APPLETEAMID,
       },
     },
   });
 };
 
-main().then((response) => {console.log('Build & Notarize complete'); response.map(x=> console.log(x))}).catch((e) => console.error(e));
+main().then((response) => { console.log('Build & Notarize complete'); response.map(x => console.log(x)) }).catch((e) => console.error(e));
