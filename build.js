c17305526cdcfa0a8a404fcd2fdd7ae504c5d67d
Josh Miller <mlxndrj@gmail.com>
2024-05-08 23:57:05 +0100

    chore: Update build workflow to use custom build script for macOS notarization

---
+++ build.js
@@ -0,0 +1,42 @@
+const dotenv = require('dotenv');
+const build = require('electron-builder').build;
+
+dotenv.config();
+
+const main = async () => {
+  console.log('Building...');
+
+  /** @type import {CliOptions} from "electron-builder" */
+  await build({
+    config: {
+      productName: 'Olas Operate',
+      artifactName: '${productName}-${version}-${platform}-${arch}.${ext}',
+      mac: {
+        appId: 'xyz.valory.olas-operate-app',
+        category: 'public.app-category.utilities',
+        directories: {
+          output: 'dist',
+        },
+        mac: {
+          target: [
+            {
+              target: 'dmg',
+              arch: ['arm64'],
+            },
+          ],
+          icon: 'electron/assets/icons/splash-robot-head.png',
+          hardenedRuntime: true,
+          gatekeeperAssess: false,
+          entitlements: 'electron/entitlements.mac.plist',
+          entitlementsInherit: 'electron/entitlements.mac.plist',
+          category: 'public.app-category.utilities',
+        },
+        notarize: {
+          teamId: process.env.APPLETEAMID,
+        },
+      },
+    },
+  });
+};
+
+main.then(() => console.log('Build & Notarize complete')).catch((e) => console.error(e));
