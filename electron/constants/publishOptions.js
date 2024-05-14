b04972639d66053109596d3b73a1d91688964ebb
Josh Miller <mlxndrj@gmail.com>
2024-05-14 10:49:14 +0100

    chore: Update publishOptions.js with new update key

--- electron/constants/publishOptions.js
+++ electron/constants/publishOptions.js
@@ -1,9 +1,13 @@
+// update key: READ-ONLY access to Olas Operate App, delete once public
+const updateKey =
+  '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';
+
 const publishOptions = {
   provider: 'github',
   owner: 'valory-xyz',
   repo: 'olas-operate-app',
   releaseType: 'release',
-  token: process.env.GH_TOKEN,
+  token: updateKey,
   private: true,
   publishAutoUpdate: true,
 };
