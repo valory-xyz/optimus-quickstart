ff7125c4d1dad47ecdd40ea120e11d8222116efe
mohandast52 <mohandast52@gmail.com>
2024-05-15 20:41:31 +0530

    chore: Update MainOlasBalance component to use Skeleton for loading state

--- frontend/components/Main/MainOlasBalance.tsx
+++ frontend/components/Main/MainOlasBalance.tsx
@@ -1,6 +1,5 @@
-import { Spin } from 'antd';
+import { Skeleton } from 'antd';
 import { useMemo } from 'react';
-import styled from 'styled-components';
 
 import { balanceFormat } from '@/common-util/numberFormatters';
 import { UNICODE_SYMBOLS } from '@/constants/unicode';
@@ -8,36 +7,25 @@ import { useBalance } from '@/hooks';
 
 import { CardSection } from '../styled/CardSection';
 
-const Loader = styled.div`
-  width: 120px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 30px;
-  padding-top: 24px;
-`;
-
 export const MainOlasBalance = () => {
   const { isBalanceLoaded, totalOlasBalance } = useBalance();
 
   const balance = useMemo(() => {
-    if (!isBalanceLoaded)
-      return (
-        <Loader>
-          <Spin />
-        </Loader>
-      );
-
     if (totalOlasBalance === undefined) return '--';
-
     return balanceFormat(totalOlasBalance, 2);
-  }, [isBalanceLoaded, totalOlasBalance]);
+  }, [totalOlasBalance]);
 
   return (
     <CardSection align="end" gap={5} borderTop borderBottom>
-      <span className="balance-symbol">{UNICODE_SYMBOLS.OLAS}</span>
-      <span className="balance">{balance}</span>
-      <span className="balance-currency">OLAS</span>
+      {!isBalanceLoaded ? (
+        <>
+          <span className="balance-symbol">{UNICODE_SYMBOLS.OLAS}</span>
+          <span className="balance">{balance}</span>
+          <span className="balance-currency">OLAS</span>
+        </>
+      ) : (
+        <Skeleton.Input active size="large" />
+      )}
     </CardSection>
   );
 };
