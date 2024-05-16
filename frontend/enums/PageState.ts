659d900c26d8f3b4f488f76bb238f30b18c0ab84
mohandast52 <mohandast52@gmail.com>
2024-05-16 14:15:11 +0530

    refactor: Update PageHeights type to use number instead of string

--- frontend/enums/PageState.ts
+++ frontend/enums/PageState.ts
@@ -6,10 +6,10 @@ export enum PageState {
   Send,
 }
 
-export const PageHeights: Record<PageState, string> = {
-  [PageState.Setup]: '500',
-  [PageState.Main]: '730',
-  [PageState.Settings]: '300',
-  [PageState.Receive]: 'auto',
-  [PageState.Send]: 'auto',
+export const PageHeights: Record<PageState, number | null> = {
+  [PageState.Setup]: 500,
+  [PageState.Main]: 730,
+  [PageState.Settings]: 300,
+  [PageState.Receive]: null,
+  [PageState.Send]: null,
 };
