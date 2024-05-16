6af7ffdbd3feef513d40696b91645321f2dabc8f
angrybayblade <vptl185@gmail.com>
2024-05-16 11:41:52 +0530

    feat: adjust frontend SDK

--- frontend/service/Wallet.ts
+++ frontend/service/Wallet.ts
@@ -16,17 +16,27 @@ const createEoa = async (chain: Chain) =>
     body: JSON.stringify({ chain_type: chain }),
   }).then((res) => res.json());
 
-const createSafe = async (chain: Chain) =>
-  fetch(`${BACKEND_URL}/wallet`, {
+const createSafe = async (chain: Chain, owner?: string) =>
+  fetch(`${BACKEND_URL}/wallet/safe`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ chain_type: chain, owner: owner }),
+  }).then((res) => res.json());
+
+const addBackupOwner = async (chain: Chain, owner: string) =>
+  fetch(`${BACKEND_URL}/wallet/safe`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ chain_type: chain }),
+    body: JSON.stringify({ chain_type: chain, owner: owner }),
   }).then((res) => res.json());
 
 export const WalletService = {
   getWallets,
   createEoa,
   createSafe,
+  addBackupOwner
 };
