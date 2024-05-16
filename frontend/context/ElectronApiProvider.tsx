e1ad455afe74942383977c162f4331a62d7da280
mohandast52 <mohandast52@gmail.com>
2024-05-16 14:13:26 +0530

    chore: Add ElectronApiProvider component for managing Electron API context

---
+++ frontend/context/ElectronApiProvider.tsx
@@ -0,0 +1,31 @@
+import { get } from 'lodash';
+import { createContext, PropsWithChildren } from 'react';
+
+type ElectronApiContextProps = {
+  setHeight?: (height: number) => void;
+};
+
+export const ElectronApiContext = createContext<ElectronApiContextProps>({
+  setHeight: undefined,
+});
+
+const setHeight = (functionNameInWindow: string) => {
+  if (typeof window === 'undefined') return;
+
+  const fn = get(window, `electronAPI.${functionNameInWindow}`);
+  if (!fn || typeof fn !== 'function') return;
+
+  return fn;
+};
+
+export const ElectronApiProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <ElectronApiContext.Provider
+      value={{
+        setHeight: setHeight('setAppHeight'),
+      }}
+    >
+      {children}
+    </ElectronApiContext.Provider>
+  );
+};
