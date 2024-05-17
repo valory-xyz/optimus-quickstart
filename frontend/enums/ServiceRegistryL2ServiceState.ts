9d96668806cb376229bdba9b83d09d1eb0cda338
Josh Miller <mlxndrj@gmail.com>
2024-05-17 17:03:41 +0100

    remove unused comments

--- frontend/enums/ServiceRegistryL2ServiceState.ts
+++ frontend/enums/ServiceRegistryL2ServiceState.ts
@@ -1,9 +1,9 @@
 // Reflects the state of a service in the ServiceRegistryL2 contract (i.e. https://gnosisscan.io/address/0xa45E64d13A30a51b91ae0eb182e88a40e9b18eD8)
 export enum ServiceRegistryL2ServiceState {
-  NonExistent, // ?
-  PreRegistration, // don't include security deposit value or bond value
-  ActiveRegistration, // include security deposit value, not bond value
-  FinishedRegistration, // include security deposit value and bond value
-  Deployed, // ?
-  TerminatedBonded, // ?
+  NonExistent,
+  PreRegistration,
+  ActiveRegistration,
+  FinishedRegistration,
+  Deployed,
+  TerminatedBonded,
 }
