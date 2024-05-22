bbcd9bec38b4bc3f3233cc950d6a1b7a38b89df5
mohandast52 <mohandast52@gmail.com>
2024-05-22 18:30:21 +0530

    chore: Refactor MainRewards component to improve UI and add staked amount display

--- frontend/components/Main/MainRewards.tsx
+++ frontend/components/Main/MainRewards.tsx
@@ -1,12 +1,20 @@
 import { Col, Flex, Row, Skeleton, Tag, Typography } from 'antd';
+import styled from 'styled-components';
 
 import { balanceFormat } from '@/common-util';
+import { COLOR } from '@/constants';
 import { useBalance } from '@/hooks';
 import { useReward } from '@/hooks/useReward';
 
-import { CardSection } from '../styled/CardSection';
-
 const { Text } = Typography;
+
+const RewardsRow = styled(Row)`
+  margin: 0 -24px;
+  > .ant-col {
+    padding: 24px;
+    border-right: 1px solid ${COLOR.BORDER_GRAY};
+  }
+`;
 
 // TODO: Replace with real data
 const isStakingInfoLoaded = false;
@@ -17,50 +25,48 @@ export const MainRewards = () => {
   const { isBalanceLoaded, totalOlasStakedBalance } = useBalance();
 
   return (
-    <CardSection gap={5} vertical>
-      <Row justify="space-between">
-        <Col span={12}>
-          <Flex vertical gap={4} align="flex-start">
-            <Text>Staking rewards today</Text>
-            {isBalanceLoaded ? (
-              <>
-                <Text strong style={{ fontSize: 20 }}>
-                  {balanceFormat(availableRewardsForEpochEther, 2)} OLAS
-                </Text>
-                {isEligibleForRewards ? (
-                  <Tag color="success">Earned</Tag>
-                ) : (
-                  <Tag color="processing">Not yet earned</Tag>
-                )}
-              </>
-            ) : (
-              <Flex vertical gap={8}>
-                <Skeleton.Button active size="small" style={{ width: 92 }} />
-                <Skeleton.Button active size="small" style={{ width: 92 }} />
-              </Flex>
-            )}
-          </Flex>
-        </Col>
+    <RewardsRow>
+      <Col span={12}>
+        <Flex vertical gap={4} align="flex-start">
+          <Text>Staking rewards today</Text>
+          {isBalanceLoaded ? (
+            <>
+              <Text strong style={{ fontSize: 20 }}>
+                {balanceFormat(availableRewardsForEpochEther, 2)} OLAS
+              </Text>
+              {isEligibleForRewards ? (
+                <Tag color="success">Earned</Tag>
+              ) : (
+                <Tag color="processing">Not yet earned</Tag>
+              )}
+            </>
+          ) : (
+            <Flex vertical gap={8}>
+              <Skeleton.Button active size="small" style={{ width: 92 }} />
+              <Skeleton.Button active size="small" style={{ width: 92 }} />
+            </Flex>
+          )}
+        </Flex>
+      </Col>
 
-        <Col span={12}>
-          <Flex vertical gap={4} align="flex-start">
-            <Text>Staked amount</Text>
-            {isStakingInfoLoaded ? (
-              <>
-                <Text strong style={{ fontSize: 20 }}>
-                  {balanceFormat(totalOlasStakedBalance, 2)} OLAS
-                </Text>
-                {isStaked ? null : <Tag color="processing">Not yet staked</Tag>}
-              </>
-            ) : (
-              <Flex vertical gap={8}>
-                <Skeleton.Button active size="small" style={{ width: 92 }} />
-                <Skeleton.Button active size="small" style={{ width: 92 }} />
-              </Flex>
-            )}
-          </Flex>
-        </Col>
-      </Row>
-    </CardSection>
+      <Col span={12}>
+        <Flex vertical gap={4} align="flex-start">
+          <Text>Staked amount</Text>
+          {isStakingInfoLoaded ? (
+            <>
+              <Text strong style={{ fontSize: 20 }}>
+                {balanceFormat(totalOlasStakedBalance, 2)} OLAS
+              </Text>
+              {isStaked ? null : <Tag color="processing">Not yet staked</Tag>}
+            </>
+          ) : (
+            <Flex vertical gap={8}>
+              <Skeleton.Button active size="small" style={{ width: 92 }} />
+              <Skeleton.Button active size="small" style={{ width: 92 }} />
+            </Flex>
+          )}
+        </Flex>
+      </Col>
+    </RewardsRow>
   );
 };
