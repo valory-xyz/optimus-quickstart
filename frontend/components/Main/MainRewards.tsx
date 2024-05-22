dfa4272439cf32c365e3595a9e2be4080895b48e
mohandast52 <mohandast52@gmail.com>
2024-05-22 20:03:35 +0530

    chore: Modal & notification added

--- frontend/components/Main/MainRewards.tsx
+++ frontend/components/Main/MainRewards.tsx
@@ -1,5 +1,6 @@
-import { Col, Flex, Row, Skeleton, Tag, Typography } from 'antd';
-import { useEffect } from 'react';
+import { Button, Col, Flex, Modal, Row, Skeleton, Tag, Typography } from 'antd';
+import Image from 'next/image';
+import { useEffect, useMemo } from 'react';
 import styled from 'styled-components';
 
 import { balanceFormat } from '@/common-util';
@@ -8,7 +9,7 @@ import { useBalance } from '@/hooks';
 import { useElectronApi } from '@/hooks/useElectronApi';
 import { useReward } from '@/hooks/useReward';
 
-const { Text } = Typography;
+const { Text, Title } = Typography;
 
 const RewardsRow = styled(Row)`
   margin: 0 -24px;
@@ -75,21 +76,79 @@ const DisplayRewards = () => {
 
 const NotifyRewards = () => {
   const { isEligibleForRewards, availableRewardsForEpochEther } = useReward();
+  const { totalOlasBalance } = useBalance();
   const { showNotification } = useElectronApi();
 
-  useEffect(() => {
-    const hasAlreadyNotified = false; // TODO: Implement this
+  const canShowNotification = useMemo(() => {
+    const hasAlreadyNotified = false; // TODO: Implement this once state persistence is available
 
-    if (hasAlreadyNotified) return;
-    if (!isEligibleForRewards) return;
+    if (hasAlreadyNotified) return false;
+    if (!isEligibleForRewards) return false;
+
+    return isEligibleForRewards && availableRewardsForEpochEther;
+  }, [isEligibleForRewards, availableRewardsForEpochEther]);
+
+  useEffect(() => {
+    if (!canShowNotification) return;
 
     showNotification?.(
       'Your agent earned its first staking rewards!',
       `Congratulations! Your agent just got the first reward for you! Your current balance: ${availableRewardsForEpochEther} OLAS`,
     );
-  }, [isEligibleForRewards, availableRewardsForEpochEther, showNotification]);
+    // TODO: setter for hasAlreadyNotified
+  }, [canShowNotification, availableRewardsForEpochEther, showNotification]);
 
-  return null;
+  // if (!canShowNotification) return null;
+
+  return (
+    <Modal
+      open
+      width={400}
+      footer={[
+        <Button
+          key="back"
+          // disabled
+          type="primary"
+          block
+          size="large"
+          style={{ marginTop: 16 }}
+        >
+          <Flex align="center" justify="center" gap={2}>
+            Share on
+            <Image src="/twitter.svg" width={24} height={24} alt="OLAS logo" />
+          </Flex>
+        </Button>,
+      ]}
+    >
+      <Flex align="center" justify="center">
+        <Image
+          src="/splash-robot-head.png"
+          width={100}
+          height={100}
+          alt="OLAS logo"
+        />
+      </Flex>
+
+      <Title level={5}>Your agent just earned the first reward!</Title>
+
+      <Flex vertical gap={16}>
+        <Text>
+          Congratulations! Your agent just earned the first
+          <Text strong>
+            {` ${balanceFormat(availableRewardsForEpochEther, 2)} OLAS `}
+          </Text>
+          for you!
+        </Text>
+
+        <Text>
+          Your current balance:
+          <Text strong>{` ${balanceFormat(totalOlasBalance, 2)} OLAS `}</Text>
+        </Text>
+
+        <Text>Keep it running to get even more!</Text>
+      </Flex>
+    </Modal>
+  );
 };
 
 export const MainRewards = () => (
