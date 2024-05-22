b448b4e4541f548cc113219e85bb1d6ba0bc3b5b
mohandast52 <mohandast52@gmail.com>
2024-05-22 17:17:23 +0530

    chore: Refactor MainRewards component to improve UI and add staked amount display

--- frontend/components/Main/MainRewards.tsx
+++ frontend/components/Main/MainRewards.tsx
@@ -1,6 +1,4 @@
-import { Flex, Skeleton, Tag, Typography } from 'antd';
-import { memo } from 'react';
-import styled from 'styled-components';
+import { Col, Flex, Row, Skeleton, Tag, Typography } from 'antd';
 
 import { balanceFormat } from '@/common-util';
 import { useBalance } from '@/hooks';
@@ -10,43 +8,59 @@ import { CardSection } from '../styled/CardSection';
 
 const { Text } = Typography;
 
-const Loader = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 14px;
-  margin-top: 4px;
-`;
-
 export const MainRewards = () => {
   const { availableRewardsForEpochEther, isEligibleForRewards } = useReward();
   const { isBalanceLoaded } = useBalance();
 
+  const isStakingInfoLoaded = false;
+  const isStaked = false;
+  const stakedAmount = 10;
+
   return (
     <CardSection gap={5} vertical>
-      <Text>Staking rewards today </Text>
-      {isBalanceLoaded ? (
-        <Flex gap={10}>
-          <Text strong>
-            {balanceFormat(availableRewardsForEpochEther, 2)} OLAS
-          </Text>
-          <RewardsEarned isEligibleForRewards={isEligibleForRewards} />
-        </Flex>
-      ) : (
-        <Loader>
-          <Skeleton.Button active size="small" style={{ width: 92 }} />
-          <Skeleton.Button active size="small" style={{ width: 92 }} />
-        </Loader>
-      )}
+      <Row justify="space-between">
+        <Col span={12}>
+          <Flex vertical gap={4} align="flex-start">
+            <Text>Staking rewards today</Text>
+            {isBalanceLoaded ? (
+              <>
+                <Text strong style={{ fontSize: 20 }}>
+                  {balanceFormat(availableRewardsForEpochEther, 2)} OLAS
+                </Text>
+                {isEligibleForRewards ? (
+                  <Tag color="success">Earned</Tag>
+                ) : (
+                  <Tag color="processing">Not yet earned</Tag>
+                )}
+              </>
+            ) : (
+              <Flex vertical gap={8}>
+                <Skeleton.Button active size="small" style={{ width: 92 }} />
+                <Skeleton.Button active size="small" style={{ width: 92 }} />
+              </Flex>
+            )}
+          </Flex>
+        </Col>
+
+        <Col span={12}>
+          <Flex vertical gap={4} align="flex-start">
+            <Text>Staked amount</Text>
+            {isStakingInfoLoaded ? (
+              <>
+                <Text strong style={{ fontSize: 20 }}>
+                  {balanceFormat(stakedAmount, 2)} OLAS
+                </Text>
+                {isStaked ? null : <Tag color="processing">Not yet staked</Tag>}
+              </>
+            ) : (
+              <Flex vertical gap={8}>
+                <Skeleton.Button active size="small" style={{ width: 92 }} />
+                <Skeleton.Button active size="small" style={{ width: 92 }} />
+              </Flex>
+            )}
+          </Flex>
+        </Col>
+      </Row>
     </CardSection>
   );
 };
-
-const RewardsEarned = memo(function RewardsEarned({
-  isEligibleForRewards,
-}: {
-  isEligibleForRewards?: boolean;
-}) {
-  if (!isEligibleForRewards)
-    return <Tag color="processing">Not yet earned</Tag>;
-  return <Tag color="success">Earned</Tag>;
-});
