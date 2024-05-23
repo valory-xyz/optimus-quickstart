c31ed714aad782a444650a13544cfb570465375b
mohandast52 <mohandast52@gmail.com>
2024-05-23 05:39:42 +0530

    refactor: Refactor Confetti component to use hooks and improve performance

--- frontend/common-util/Confetti.js
+++ frontend/common-util/Confetti.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactCanvasConfetti from 'react-canvas-confetti';
+import { useInterval } from 'usehooks-ts';
 
 const canvasStyles = {
   position: 'fixed',
@@ -10,63 +11,31 @@ const canvasStyles = {
   left: 0,
 };
 
-export default class Realistic extends React.Component {
-  constructor(props) {
-    super(props);
-    this.animationInstance = null;
-  }
+export const Confetti = () => {
+  const animationInstance = useRef(null);
 
-  makeShot = (particleRatio, opts) => {
-    this.animationInstance &&
-      this.animationInstance({
+  const makeShot = (particleRatio, opts) => {
+    animationInstance &&
+      animationInstance.current({
         ...opts,
-        origin: { y: 0.7 },
+        origin: { y: 0.45 },
         particleCount: Math.floor(200 * particleRatio),
       });
   };
 
-  fire = () => {
-    this.makeShot(0.25, {
-      spread: 26,
-      startVelocity: 55,
-    });
-
-    this.makeShot(0.2, {
-      spread: 60,
-    });
-
-    this.makeShot(0.35, {
-      spread: 100,
-      decay: 0.91,
-      scalar: 0.8,
-    });
-
-    this.makeShot(0.1, {
-      spread: 120,
-      startVelocity: 25,
-      decay: 0.92,
-      scalar: 1.2,
-    });
-
-    this.makeShot(0.1, {
-      spread: 120,
-      startVelocity: 45,
-    });
+  const fire = () => {
+    makeShot(0.25, { spread: 26, startVelocity: 55 });
+    makeShot(0.2, { spread: 60 });
+    makeShot(0.35, { spread: 100, decay: 0.91, scalar: 0.8 });
+    makeShot(0.1, { spread: 120, startVelocity: 25, decay: 0.92, scalar: 1.2 });
+    makeShot(0.1, { spread: 120, startVelocity: 45 });
   };
 
-  getInstance = (instance) => {
-    this.animationInstance = instance;
+  const getInstance = (instance) => {
+    animationInstance.current = instance;
   };
 
-  render() {
-    return (
-      <>
-        <button onClick={this.fire}>Fire</button>
-        <ReactCanvasConfetti
-          refConfetti={this.getInstance}
-          style={canvasStyles}
-        />
-      </>
-    );
-  }
-}
+  useInterval(() => fire(), 2500);
+
+  return <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />;
+};
