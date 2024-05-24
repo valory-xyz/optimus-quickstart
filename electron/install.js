dd0296c8c3d71d9b09d07ada76a8ee172366df7d
angrybayblade <vptl185@gmail.com>
2024-05-24 16:17:54 +0530

    feat: bump to v0.1.0-rc22

--- electron/install.js
+++ electron/install.js
@@ -7,7 +7,7 @@ const process = require('process');
 const { spawnSync } = require('child_process');
 const Docker = require('dockerode');
 
-const Version = '0.1.0rc21';
+const Version = '0.1.0rc22';
 const OperateDirectory = `${os.homedir()}/.operate`;
 const VenvDir = `${OperateDirectory}/venv`;
 const VersionFile = `${OperateDirectory}/version.txt`;
