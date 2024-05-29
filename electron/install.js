4fe78d77fcab87a0cc2ad07bc4d30fc697ea06d5
angrybayblade <vptl185@gmail.com>
2024-05-29 19:23:48 +0530

    feat: bump to v0.1.0-rc27

--- electron/install.js
+++ electron/install.js
@@ -11,7 +11,7 @@ const axios = require("axios")
 const Docker = require('dockerode');
 const { spawnSync } = require('child_process');
 
-const Version = '0.1.0rc26';
+const Version = '0.1.0rc27';
 const OperateDirectory = `${os.homedir()}/.operate`;
 const VenvDir = `${OperateDirectory}/venv`;
 const TempDir = `${OperateDirectory}/temp`;
