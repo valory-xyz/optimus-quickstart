9c77e1cba6a7c849c13e5b5826ccb2ea29f5ef49
mohandast52 <mohandast52@gmail.com>
2024-05-31 19:34:06 +0530

    chore: Refactor StakingValidations component and fetchStakingContractInfo function

--- frontend/components/Main/StakingValidations.tsx
+++ frontend/components/Main/StakingValidations.tsx
@@ -1,14 +1,70 @@
+import { Flex, Typography } from 'antd';
 import { useEffect } from 'react';
 
-import { AutonolasService } from '@/service/Autonolas';
+import { Alert } from '../common/Alert';
+import { useStakingContractInfo } from '../store/stackingContractInfo';
+import { CardSection } from '../styled/CardSection';
+
+const { Text, Paragraph } = Typography;
+
+const COVER_PREV_BLOCK_BORDER_STYLE = { marginBottom: '-1px' };
 
 export const StakingValidations = () => {
+  const {
+    hasEnoughServiceSlots,
+    isRewardsAvailable,
+    isStakingContractInfoLoading,
+    fetchStakingContractInfo,
+  } = useStakingContractInfo();
+
   useEffect(() => {
-    const getRewards = async () => {
-      await AutonolasService.getStakingContractInfo();
-    };
-    getRewards();
-  }, []);
+    fetchStakingContractInfo();
+  }, [fetchStakingContractInfo]);
+
+  if (isStakingContractInfoLoading) return null;
+
+  if (!isRewardsAvailable) {
+    return (
+      <CardSection style={COVER_PREV_BLOCK_BORDER_STYLE}>
+        <Alert
+          showIcon
+          message={
+            <Flex vertical gap={4}>
+              <Text className="font-weight-600 mb-4">No rewards available</Text>
+              <Paragraph className="mb-4">
+                There are no rewards available for staking.
+              </Paragraph>
+            </Flex>
+          }
+          type="error"
+          fullWidth
+        />
+      </CardSection>
+    );
+  }
+
+  if (!hasEnoughServiceSlots) {
+    return (
+      <CardSection style={COVER_PREV_BLOCK_BORDER_STYLE}>
+        <Alert
+          showIcon
+          message={
+            <Flex vertical gap={4}>
+              <Text className="font-weight-600 mb-4">
+                Not enough service slots
+              </Text>
+              <Paragraph className="mb-4">
+                You have reached the maximum number of services allowed for
+                staking.
+              </Paragraph>
+            </Flex>
+          }
+          type="error"
+          fullWidth
+        />
+      </CardSection>
+    );
+  }
 
   return null;
 };
--- frontend/components/store/stackingContractInfo.ts
+++ frontend/components/store/stackingContractInfo.ts
@@ -1,46 +1,49 @@
-import { useCallback, useEffect } from 'react';
 import { create } from 'zustand';
 
 import { AutonolasService } from '@/service/Autonolas';
+
+const initialState = {
+  isStakingContractInfoLoading: true,
+  isRewardsAvailable: false,
+  hasEnoughServiceSlots: false,
+  maxNumServices: 0,
+};
 
 export const useStakingContractInfo = create<{
   isStakingContractInfoLoading: boolean;
   isRewardsAvailable: boolean;
   hasEnoughServiceSlots: boolean;
+  fetchStakingContractInfo: () => Promise<void>;
+  maxNumServices: number;
 }>((set) => {
-  const fetchStakingContractInfo = useCallback(async () => {
-    try {
-      set({ isStakingContractInfoLoading: true });
-      const info = await AutonolasService.getStakingContractInfo();
-
-      if (!info) return;
-
-      const { availableRewards, maxNumServices, getServiceIds } = info;
-
-      set({
-        // availableRewards,
-        // maxNumServices,
-        // getServiceIds,
-        isRewardsAvailable: availableRewards > 0,
-        hasEnoughServiceSlots: getServiceIds.length < maxNumServices,
-      });
-    } catch (error) {
-      console.error('Failed to fetch staking contract info', error);
-    } finally {
-      set({ isStakingContractInfoLoading: false });
-    }
-  }, [set]);
-
-  useEffect(() => {
-    fetchStakingContractInfo();
-  }, [fetchStakingContractInfo]);
-
   return {
     // maxNumServices: null,
     // availableRewards: null,
     // getServiceIds: null,
+    ...initialState,
     isStakingContractInfoLoading: true,
     isRewardsAvailable: false,
-    hasEnoughServiceSlots: false,
+    fetchStakingContractInfo: async () => {
+      try {
+        set({ isStakingContractInfoLoading: true });
+        const info = await AutonolasService.getStakingContractInfo();
+
+        if (!info) return;
+
+        const { availableRewards, maxNumServices, getServiceIds } = info;
+
+        set({
+          // availableRewards,
+          maxNumServices,
+          // getServiceIds,
+          isRewardsAvailable: availableRewards > 0,
+          hasEnoughServiceSlots: getServiceIds.length < maxNumServices,
+        });
+      } catch (error) {
+        console.error('Failed to fetch staking contract info', error);
+      } finally {
+        set({ isStakingContractInfoLoading: false });
+      }
+    },
   };
 });
