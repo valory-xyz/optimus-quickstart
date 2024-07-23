ee0db5afeb7abfc6733a264f6ad13cba6a1398bb
sarthak_dev <sarthak@solulab.co>
2024-07-23 20:07:27 +0530

    add download binaries script

---
+++ download_binaries.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ARCH=$1
+BIN_DIR="electron/bins/"
+mkdir -p $BIN_DIR
+
+trader_version=$(poetry run python -c "import yaml; config = yaml.safe_load(open('templates/trader.yaml')); print(config['configuration']['trader_version'])")
+curl -L -o "${BIN_DIR}aea_bin" "https://github.com/valory-xyz/trader/releases/download/${trader_version}/trader_bin_${ARCH}"
