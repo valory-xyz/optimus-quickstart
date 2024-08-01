21d38f9b23053284ddda5a2869d6a2874f779986
Ardian <ardianabazi09@gmail.com>
2024-08-01 19:35:31 +0200

    release: rc97

--- electron/install.js
+++ electron/install.js
@@ -14,7 +14,7 @@ const { paths } = require('./constants');
  * - use "" (nothing as a suffix) for latest release candidate, for example "0.1.0rc26"
  * - use "alpha" for alpha release, for example "0.1.0rc26-alpha"
  */
-const OlasMiddlewareVersion = '0.1.0rc95';
+const OlasMiddlewareVersion = '0.1.0rc97';
 
 const Env = {
   ...process.env,
