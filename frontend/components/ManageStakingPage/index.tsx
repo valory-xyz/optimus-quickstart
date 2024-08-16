207c7ce0513cb9b4779fb03bb9dca20063d1af97
truemiller <mlxndrj@gmail.com>
2024-08-16 12:10:49 +0100

    refactor: replace mocked contracts data

--- frontend/components/ManageStakingPage/index.tsx
+++ frontend/components/ManageStakingPage/index.tsx
@@ -1,33 +1,34 @@
 import { CloseOutlined } from '@ant-design/icons';
 import { Button, Card } from 'antd';
 
+import { Chain } from '@/client';
+import { SERVICE_STAKING_TOKEN_MECH_USAGE_CONTRACT_ADDRESSES } from '@/constants/contractAddresses';
 import { Pages } from '@/enums/PageState';
-import { StakingProgramStatus } from '@/enums/StakingProgramStatus';
+import { StakingProgram } from '@/enums/StakingProgram';
 import { usePageState } from '@/hooks/usePageState';
-import { StakingProgram } from '@/types/StakingProgram';
 
 import { CardTitle } from '../Card/CardTitle';
-import { StakingContract } from './StakingContract';
+import { StakingContractSection } from './StakingContract';
 import { WhatAreStakingContractsSection } from './WhatAreStakingContracts';
 
-const mockStakingContracts: StakingProgram[] = [
-  {
-    name: 'Pearl Beta',
-    rewardsPerWorkPeriod: 0.14,
-    requiredOlasForStaking: 40,
-    isEnoughSlots: true,
-    status: StakingProgramStatus.New,
-    contractAddress: '0x1234567890',
-  },
-  {
-    name: 'Pearl Alpha',
-    rewardsPerWorkPeriod: 0.047,
-    requiredOlasForStaking: 20,
-    isEnoughSlots: true,
-    status: StakingProgramStatus.Selected,
-    contractAddress: '0x0987654321',
-  },
-];
+// const mockStakingContracts: StakingProgram[] = [
+//   {
+//     name: 'Pearl Beta',
+//     rewardsPerWorkPeriod: 0.14,
+//     requiredOlasForStaking: 40,
+//     isEnoughSlots: true,
+//     status: StakingProgramStatus.New,
+//     contractAddress: '0x1234567890',
+//   },
+//   {
+//     name: 'Pearl Alpha',
+//     rewardsPerWorkPeriod: 0.047,
+//     requiredOlasForStaking: 20,
+//     isEnoughSlots: true,
+//     status: StakingProgramStatus.Selected,
+//     contractAddress: '0x0987654321',
+//   },
+// ];
 
 export const ManageStakingPage = () => {
   const { goto } = usePageState();
@@ -44,8 +45,14 @@ export const ManageStakingPage = () => {
       }
     >
       <WhatAreStakingContractsSection />
-      {mockStakingContracts.map((contract) => (
-        <StakingContract key={contract.contractAddress} contract={contract} />
+      {Object.entries(
+        SERVICE_STAKING_TOKEN_MECH_USAGE_CONTRACT_ADDRESSES[Chain.GNOSIS],
+      ).map(([stakingProgram, contractAddress]) => (
+        <StakingContractSection
+          key={contractAddress}
+          stakingProgram={stakingProgram as StakingProgram}
+          contractAddress={contractAddress}
+        />
       ))}
     </Card>
   );
