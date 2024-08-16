50d688bc0496472f21c6df19e1b89898615c28fd
truemiller <mlxndrj@gmail.com>
2024-08-16 12:11:54 +0100

    refactor: use stakingContractMeta in staking contract sections

--- frontend/components/SettingsPage/StakingContractSection.tsx
+++ frontend/components/SettingsPage/StakingContractSection.tsx
@@ -1,5 +1,7 @@
 import { Button, Flex, Typography } from 'antd';
 
+import { Chain } from '@/client';
+import { SERVICE_STAKING_TOKEN_MECH_USAGE_CONTRACT_ADDRESSES } from '@/constants/contractAddresses';
 import { UNICODE_SYMBOLS } from '@/constants/symbols';
 import { Pages } from '@/enums/PageState';
 import { usePageState } from '@/hooks/usePageState';
@@ -11,14 +13,24 @@ const { Text } = Typography;
 
 export const StakingContractSection = () => {
   const { goto } = usePageState();
-  const { currentStakingProgram } = useStakingProgram();
+  const {
+    currentStakingProgram,
+    currentStakingProgramMeta,
+    defaultStakingProgram,
+  } = useStakingProgram();
+
+  const stakingContractAddress =
+    SERVICE_STAKING_TOKEN_MECH_USAGE_CONTRACT_ADDRESSES[Chain.GNOSIS][
+      currentStakingProgram ?? defaultStakingProgram
+    ];
+
   return (
     <CardSection vertical gap={8} align="start" borderbottom="true">
       <Text strong>Staking contract</Text>
       <Flex gap={16}>
-        <Text type="secondary">{currentStakingProgram.name}</Text>
+        <Text type="secondary">{currentStakingProgramMeta.name}</Text>
         <a
-          href={`https://gnosisscan.io/address/${currentStakingProgram.contractAddress}`}
+          href={`https://gnosisscan.io/address/${stakingContractAddress}`}
           target="_blank"
         >
           Contract details {UNICODE_SYMBOLS.EXTERNAL_LINK}
