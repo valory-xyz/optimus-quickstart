8508774eb3e87685027c59aa1a5feec71bfa6f9d
gauravlochab <gauravlochab1997@gmail.com>
2024-10-04 18:52:43 +0530

    Update service template hash

--- run_service.py
+++ run_service.py
@@ -358,7 +358,7 @@ def get_service_template(config: OptimusConfig) -> ServiceTemplate:
     """Get the service template"""
     return ServiceTemplate({
         "name": "Optimus",
-        "hash": "bafybeibn44yqjs7f2inqhmcl3jginx7xdgkk7lf4gduatqval3hpqkz6li",
+        "hash": "bafybeiatudbv3fluo5o5h4kldm6em47l4tyjpy73q2bwuijbzxhckzyrxy",
 
         "description": "Optimus",
         "image": "https://operate.olas.network/_next/image?url=%2Fimages%2Fprediction-agent.png&w=3840&q=75",
