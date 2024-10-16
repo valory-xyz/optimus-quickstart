5cdf0d21c864c71e1b8d90f131a470142b3fb30d
Divya-Solulab <divya.r@solulab.co>
2024-10-16 13:44:11 +0530

    fix: update service hash

--- run_service.py
+++ run_service.py
@@ -381,7 +381,7 @@ def get_service_template(config: OptimusConfig) -> ServiceTemplate:
     """Get the service template"""
     return ServiceTemplate({
         "name": "Optimus",
-        "hash": "bafybeige2z6ek4qictsutpyc4njfokrabvm3w7fffq343iltf5xssxzol4",
+        "hash": "bafybeifdjaennczbswrpzgfty6gvwbhf4xx2dbizir5mdzcjpb6k3zhloy",
 
         "description": "Optimus",
         "image": "https://operate.olas.network/_next/image?url=%2Fimages%2Fprediction-agent.png&w=3840&q=75",
