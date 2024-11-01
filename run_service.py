9b93a98722a41eef416694f504683b803c57989c
David Vilela <dvilelaf@gmail.com>
2024-11-01 11:53:46 +0100

    feat: custom persona

--- run_service.py
+++ run_service.py
@@ -139,6 +139,7 @@ class MemeooorrConfig(LocalResource):
     min_feedback_replies: t.Optional[str] = None
     total_supply: t.Optional[str] = None
     deployment_amount_eth: t.Optional[str] = None
+    persona: t.Optional[str] = None
 
     @classmethod
     def from_json(cls, obj: t.Dict) -> "LocalResource":
@@ -307,6 +308,10 @@ def get_local_config() -> MemeooorrConfig:
 
     if memeooorr_config.deployment_amount_eth is None:
         memeooorr_config.deployment_amount_eth = input_with_default_value("What's the amount of ETH that you want to invest in each token? (0.01 min)", 0.01)
+
+    if memeooorr_config.persona is None:
+        memeooorr_config.persona = input_with_default_value("What's the agent persona", "a cat lover that is crazy about all-things cats")
+
 
     memeooorr_config.store()
     return memeooorr_config
@@ -616,6 +621,7 @@ def main() -> None:
         "MIN_FEEDBACK_REPLIES": memeooorr_config.min_feedback_replies,
         "TOTAL_SUPPLY": memeooorr_config.total_supply,
         "DEPLOYMENT_AMOUNT_ETH": memeooorr_config.deployment_amount_eth,
+        "PERSONA": memeooorr_config.persona,
         "RESET_PAUSE_DURATION": 10,
         "MEME_FACTORY_ADDRESS": "0x1Aa15a8A751c601BbE31390dbb8711013BFD013d",
         "MINIMUM_GAS_BALANCE": 0.02,
