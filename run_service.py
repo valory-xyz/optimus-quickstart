8e4ff2bb87a47338afbab94c4bc6f3b3f0c44e61
Divya-Solulab <divya.r@solulab.co>
2024-11-28 15:05:07 +0530

    fix: update service hash

--- run_service.py
+++ run_service.py
@@ -458,7 +458,7 @@ def get_service_template(config: OptimusConfig) -> ServiceTemplate:
     home_chain_id = "10" if config.staking_chain == "optimism" else "34443"
     return ServiceTemplate({
         "name": "Optimus",
-        "hash": "bafybeifdnw7yccrkygtldtd47bmp7bucvjbeqqxodnqphn56geyeodwe7a",
+        "hash": "bafybeietdnuhihv7o5gsylbthhs5dtnyew65flqykqa7ezw6ibjljfvobu",
 
         "description": "Optimus",
         "image": "https://gateway.autonolas.tech/ipfs/bafybeiaakdeconw7j5z76fgghfdjmsr6tzejotxcwnvmp3nroaw3glgyve",
