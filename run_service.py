d678564cadc004c37c470402c72adb97864514be
Divya-Solulab <divya.r@solulab.co>
2024-12-03 21:53:49 +0530

    chore: update service hash

--- run_service.py
+++ run_service.py
@@ -507,7 +507,7 @@ def get_service_template(config: OptimusConfig) -> ServiceTemplate:
     home_chain_id = "10" if config.staking_chain == "optimism" else "34443"
     return ServiceTemplate({
         "name": "Optimus",
-        "hash": "bafybeib5amcj5lggqyo6uf7422uqzs7ozmmok4n7jaooggn7x526ihgi4a",
+        "hash": "bafybeieeex4vf4a3pj7ly563rxjrph5diylm2rr7qcvaks3ofqfvzxkjna",
 
         "description": "Optimus",
         "image": "https://gateway.autonolas.tech/ipfs/bafybeiaakdeconw7j5z76fgghfdjmsr6tzejotxcwnvmp3nroaw3glgyve",
